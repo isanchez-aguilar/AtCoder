3ea521673574c2b079f93a06843ff23031b8478f
Isaac Sanchez Aguilar <saais31@gmail.com>
2020-03-27 14:42:24 -0600

    Add files via upload

---
+++ 156A - Poor.cpp
@@ -0,0 +1,38 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: AtCoder 156A - Poor 
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <set>
+
+using namespace std;
+using Long = long long;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	set<int> vals;
+
+	for (int i = 0; i < 3; ++i)
+	{
+		int val;
+		cin >> val;
+		
+		vals.insert(val);
+	}
+
+	if (vals.size() == 2)
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+
+
+	return 0;
+}
