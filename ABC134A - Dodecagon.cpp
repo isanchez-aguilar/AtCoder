cc921170dedef60cc12108af74adfa607ab39c9a
Isaac Sanchez Aguilar <saais31@gmail.com>
2020-04-03 13:48:16 -0600

    Add files via upload

---
+++ ABC134A - Dodecagon.cpp
@@ -0,0 +1,18 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: AtCoder ABC134A - Dodecagon
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+using Long = long long;
+
+int main(void)
+{
+	Long r;
+	cin >> r;
+
+	cout << 3ll * r * r << "\n";
+
+	return 0;
+}
